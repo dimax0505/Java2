1b7fa5d1d4937c66a54c58e19bcfe79a05d52c88
dimax0505 <dimax0505@gmail.com>
2018-07-29 16:45:05 +0300

    Пока только начало, разбираюсь  с задачей

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,22 @@
 package ru.geekbrains.maksimov.Java2;
 
+import java.util.Scanner;
+
 public class MainClass {
-    
+    public static String in = "1 3 1 2\n2 3 2 2\n5 6 7 1\n3 3 1 0";
+    public static Scanner sc = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+       input_flow(in);
+        System.out.println(in);
+        sc.next();
+
+    }
+
+    public static String[][] input_flow (String str) {
+        String[][] arr =  {{"1","2"},{"3","4"}};
+        return arr;
+    }
+
 }

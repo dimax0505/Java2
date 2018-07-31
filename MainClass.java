e24f7ea3366b7d9f7efd35c3d45c26182c731dcc
dimax0505 <dimax0505@gmail.com>
2018-07-31 10:51:44 +0300

    Теперь все в руках программиста

--- MainClass.java
+++ MainClass.java
@@ -3,7 +3,7 @@ package ru.geekbrains.maksimov.Java2;
 import java.util.Scanner;
 
 public class MainClass {
-    public static String in = "52 3 1 1\n2 3 2 2\n5 1 7 1\n 3 1 0";
+    public static String in = "52 3 kjg 1\n2 3 2 2\n5 1 7 1\n3 1 0 7";
 
 
 
@@ -24,30 +24,26 @@ public class MainClass {
         String[][] arr2 = new String[arr1.length][arr1.length];
         for (int i = 0; i<arr1.length; i++){
             arr2[i] = arr1[i].split(" ");
-            if (arr2[i].length != 4) throw new MyStringExeption("В каждой строке должно быть по 4 символа");
+            if (arr2[i].length != 4) throw new MyStringExeption("В каждой строке должно быть по 4 символа, а у вас в " + (i+1) + "строке + " + (arr2[i].length)+ "символов ");
         }
         return arr2;
     }
 
-    public static double mathematics (String [][] arr) throws NumberFormatException {
+    public static double mathematics (String [][] arr) throws NumberFormatException, MyStringExeption{
         int sum = 0;
         for (int i = 0; i<arr.length; i++){
             for (int j = 0; j<arr[i].length; j++){
                 try {
                     sum += Integer.parseInt(arr[i][j]);
                 }
-               catch (NumberFormatException e){
+              catch (NumberFormatException e){
                    System.out.println("неподходящий символ: " + arr[i][j]);
                    System.out.println("находится на " + ((i)*arr.length+(j+1)) + " позиции в строке ввода");
-                  System.exit(1);
-                   // как вариант, если например надо все таки все посчитать
-                   //System.out.println("неподходящие символы не учитываем в расчетах");
-
+                  if (true) throw new MyStringExeption("Надо что то делать!!!");
                }
             }
-
         }
-            return sum/2;
+            return sum/2.0;
     }
 
 }
